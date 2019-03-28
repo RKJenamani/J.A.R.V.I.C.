9194ddcbfef64428cc0c8c5d7ef88510489218e8
Deepank308 <deepank.361998@gmail.com>
2019-03-28 19:23:41 +0530

    Add final client-server communication with a simple GUI

---
+++ Front_end/Client.java
@@ -0,0 +1,119 @@
+// Java implementation for multithreaded chat client 
+// Save file as Client.java 
+import java.io.*;
+import java.util.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.net.*;
+
+public class Client extends JFrame
+{ 
+	final static int ServerPort = 2004; 
+	InetAddress target_ip;
+	int target_port;
+	InetAddress ip;
+	JTextArea msgbox;
+	JFrame gui;
+	JTextField text;
+	int port;
+	Socket s;
+	DataOutputStream dos;
+	DataInputStream dis;
+	public static void main(String[] args) throws UnknownHostException, IOException 
+	{
+		new Client();
+
+	}
+
+	Client() throws UnknownHostException, IOException 
+	{
+		
+		try
+		{
+			ip = InetAddress.getByName("10.145.246.62"); 
+			System.out.println(ip);
+		}
+		catch
+		(UnknownHostException er)
+		{;}
+		s = new Socket(ip, ServerPort); 
+
+		dos = new DataOutputStream(s.getOutputStream()); 
+		dis = new DataInputStream(s.getInputStream()); 
+		gui = new JFrame();
+
+		msgbox= new JTextArea(100, 200);
+		msgbox.setEditable(false);
+		JScrollPane scroll= new JScrollPane(msgbox);
+
+		scroll.setBounds(100, 150, 300, 500);
+		gui.add(scroll);
+
+		JLabel l4 = new JLabel("Enter:");
+		l4.setBounds(20, 30, 60, 30);
+		gui.add(l4);
+
+		text = new JTextField();
+		text.setBounds(100, 20, 300, 50);
+		gui.add(text);
+
+		JButton send = new JButton("Send");
+		send.setBounds(200, 80, 100, 50);
+		gui.add(send);
+		gui.getRootPane().setDefaultButton(send);
+
+		send.addActionListener(new SendingListener());
+
+		Server obj = new Server();
+		Thread t = new Thread(obj);
+		t.start();
+		
+		gui.setSize(500, 800);
+		gui.setLayout(null);
+		gui.setVisible(true);
+
+	
+	}
+
+	class SendingListener implements ActionListener 
+	{
+    	public void actionPerformed(ActionEvent ae)
+    	{
+			try
+			{
+				byte[] data = new byte[1024];			
+				data = text.getText().getBytes();
+				msgbox.append("\n" + "User- " + text.getText());
+				dos.writeUTF(text.getText() + "\n"); 
+				text.setText(""); 
+			}
+			catch(Exception er)
+			{;}    			  				
+  		}
+    	
+    }
+
+    class Server implements Runnable
+    {
+    	public void run()
+    	{
+    		try
+    		{
+    			while(true)
+    			{
+    				try
+    				{
+	    				byte[] receive = new byte[1024];
+						String msg = dis.readUTF(); 
+						msgbox.append("\n" + "JARVIC- " + msg);
+    				}
+					catch(Exception er)
+    				{;}
+    			}
+    		}
+    		catch(Exception er)
+    		{;}
+    	}
+
+    }
+}
---
+++ Front_end/server.py
@@ -0,0 +1,72 @@
+# import socket programming library 
+import socket 
+import struct 
+from _thread import *
+import threading 
+
+print_lock = threading.Lock() 
+
+# thread fuction 
+def threaded_receive(c): 
+	while True: 
+
+		# data received from client 
+		data = c.recv(1024) 
+		if not data: 
+			print('Bye') 
+	        # lock released on exit 
+			print_lock.release() 
+			break
+		data = data[2:]
+		print("\nUser: " + data.decode()) 
+
+	# connection closed 
+	c.close() 
+
+
+def threaded_send(c):
+	while True:
+		msg = input("\n")
+		if msg is "\n" :
+			print("ERROR")
+			break
+		msg = msg + "\n"
+		msg = msg.encode("utf-8", 'ignore')
+		c.send(struct.pack("!H", len(msg)))
+		c.send(msg)
+	c.close()
+
+
+def Main(): 
+	host = "10.145.246.62" 
+
+	# reverse a port on your computer 
+	# in our case it is 12345 but it 
+	# can be anything 
+	port = 2004 
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+	s.bind((host, port)) 
+	print("socket binded to post", port) 
+
+	# put the socket into listening mode 
+	s.listen(5) 
+	print("socket is listening") 
+
+	# a forever loop until client wants to exit 
+	while True: 
+
+		# establish connection with client 
+		c, addr = s.accept() 
+
+		# lock acquired by client 
+		print('Connected to :', addr[0], ':', addr[1]) 
+
+		print_lock.acquire() 
+
+		# Start a new thread and return its identifier
+		start_new_thread(threaded_send,(c,))
+		start_new_thread(threaded_receive, (c,))
+	s.close()
+
+if __name__ == '__main__': 
+	Main() 
